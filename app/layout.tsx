bac824086db61875ff65b4cbe41365440ff8c296
Great Ogheneochuko <greatochuko123@gmail.com>
2025-02-04 02:47:02 +0100

    add footer component to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
+import Footer from "../components/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,11 +27,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased text-zinc-800`}
+        className={`${geistSans.variable} ${geistMono.variable} text-zinc-800 antialiased`}
       >
         <Header />
-
         {children}
+        <Footer />
       </body>
     </html>
   );
---
+++ components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="bg-zinc-900 p-6">
+      <p className="text-center text-sm text-zinc-400">
+        &copy; {new Date().getFullYear()} FinTrack by Great Ogheneochuko
+      </p>
+    </footer>
+  );
+}
