f2696c6903c46a90a9cdf01acf603972f2662239
Great Ogheneochuko <greatochuko123@gmail.com>
2025-02-05 09:26:57 +0100

    update connectDB.ts file

--- db/connectDB.ts
+++ db/connectDB.ts
@@ -3,29 +3,36 @@ import mongoose from "mongoose";
 const MONGODB_URI = process.env.MONGODB_URI;
 
 if (!MONGODB_URI) {
-  throw new Error(
-    "Please define the MONGODB_URI environment variable in .env.local",
-  );
+  throw new Error("Please define the MONGODB_URI environment variable");
 }
-// @ts-expect-error mongoose
+
+// @ts-expect-error typeof globalThis' has no index signature
 let cached = global.mongoose;
 
 if (!cached) {
-  // @ts-expect-error mongoose
+  // @ts-expect-error typeof globalThis' has no index signature
   cached = global.mongoose = { conn: null, promise: null };
 }
 
-const connectDb = async () => {
+async function connectDB() {
   if (cached.conn) {
     return cached.conn;
   }
 
   if (!cached.promise) {
-    cached.promise = mongoose.connect(MONGODB_URI).then((mongoose) => mongoose);
+    const opts = {
+      bufferCommands: false,
+    };
+
+    cached.promise = mongoose
+      .connect(process.env.MONGODB_URI!, opts)
+      .then((mongoose) => {
+        return mongoose;
+      });
   }
 
   cached.conn = await cached.promise;
   return cached.conn;
-};
+}
 
-export default connectDb;
+export default connectDB;
